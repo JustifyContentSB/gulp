42897816c93d34c457986ebf0ce3581516acfce1
rxpro <rpro700@mail.ru>
2023-06-28 03:03:28 +0300

    Верстка 3 страниц

--- sources/src/js/main.js
+++ sources/src/js/main.js
@@ -2,8 +2,39 @@
 
 $(document).ready(function () {
    // Your code here
-
+   showDropdownBlock();
+   reviewsSlider();
+   videosSlider();
 });
 
 // Your functions here
 
+function showDropdownBlock() {
+   $('[data-dropdown]').on('click', function(e) {
+      e.preventDefault();
+      $(this).toggleClass('is-active');
+      $(this).next().slideToggle(300);
+   });
+}
+
+function reviewsSlider() {
+   var swiper = new Swiper(".reviews__container", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      navigation: {
+         nextEl: ".reviews-button-next",
+         prevEl: ".reviews-button-prev",
+      },
+   });
+}
+
+function videosSlider() {
+   var swiper = new Swiper(".videos__container", {
+      slidesPerView: "auto",
+      spaceBetween: 20,
+      navigation: {
+         nextEl: ".videos-button-next",
+         prevEl: ".videos-button-prev",
+      },
+   });
+}
